cc9b7a0f3548ebe5fc46447bb6a4957e55077bef
Satomo <satrioutomo344@gmail.com>
2025-03-03 20:49:41 +0700

    Add files via upload

--- pekerjaan.sql
+++ pekerjaan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 03, 2025 at 05:17 AM
+-- Generation Time: Mar 03, 2025 at 02:48 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -39,8 +39,10 @@ CREATE TABLE `gaji` (
 --
 
 INSERT INTO `gaji` (`ID_Gaji`, `ID_Karyawan`, `Gaji_Pokok`, `Tunjangan`) VALUES
-(1, 1, 5000.00, 10000.00),
-(2, 2, 2500.00, 10000.00);
+(1, 1, 5000000.00, 1000000.00),
+(2, 2, 6000000.00, 1200000.00),
+(3, 3, 4500000.00, 900000.00),
+(4, 4, 7500000.00, 1500000.00);
 
 -- --------------------------------------------------------
 
@@ -59,8 +61,10 @@ CREATE TABLE `karyawan` (
 --
 
 INSERT INTO `karyawan` (`ID`, `Nama`, `Jabatan`) VALUES
-(1, 'Monsieur Neuvillette', 'CEO'),
-(2, 'Il Capitano', 'Co - CEO');
+(1, 'Neuvillette', 'CEO'),
+(2, 'Furina', 'Manager'),
+(3, 'Wriothesley', 'Guard'),
+(4, 'Clorinde', 'Guard');
 
 --
 -- Indexes for dumped tables
@@ -71,13 +75,29 @@ INSERT INTO `karyawan` (`ID`, `Nama`, `Jabatan`) VALUES
 --
 ALTER TABLE `gaji`
   ADD PRIMARY KEY (`ID_Gaji`),
-  ADD KEY `gaji_ibfk_1` (`ID_Karyawan`);
+  ADD KEY `ID_Karyawan` (`ID_Karyawan`);
 
 --
 -- Indexes for table `karyawan`
 --
 ALTER TABLE `karyawan`
   ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `gaji`
+--
+ALTER TABLE `gaji`
+  MODIFY `ID_Gaji` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `karyawan`
+--
+ALTER TABLE `karyawan`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
@@ -87,7 +107,7 @@ ALTER TABLE `karyawan`
 -- Constraints for table `gaji`
 --
 ALTER TABLE `gaji`
-  ADD CONSTRAINT `gaji_ibfk_1` FOREIGN KEY (`ID_Karyawan`) REFERENCES `karyawan` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `gaji_ibfk_1` FOREIGN KEY (`ID_Karyawan`) REFERENCES `karyawan` (`ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

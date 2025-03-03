257f0b6cb76dca197834dac58680d684309c5cde
Satomo <satrioutomo344@gmail.com>
2025-03-03 11:18:04 +0700

    Add files via upload

--- pekerjaan.sql
+++ pekerjaan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 03, 2025 at 04:36 AM
+-- Generation Time: Mar 03, 2025 at 05:17 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,74 @@ SET time_zone = "+00:00";
 --
 -- Database: `pekerjaan`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gaji`
+--
+
+CREATE TABLE `gaji` (
+  `ID_Gaji` int(11) NOT NULL,
+  `ID_Karyawan` int(11) DEFAULT NULL,
+  `Gaji_Pokok` decimal(10,2) DEFAULT NULL,
+  `Tunjangan` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `gaji`
+--
+
+INSERT INTO `gaji` (`ID_Gaji`, `ID_Karyawan`, `Gaji_Pokok`, `Tunjangan`) VALUES
+(1, 1, 5000.00, 10000.00),
+(2, 2, 2500.00, 10000.00);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `karyawan`
+--
+
+CREATE TABLE `karyawan` (
+  `ID` int(11) NOT NULL,
+  `Nama` varchar(100) DEFAULT NULL,
+  `Jabatan` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `karyawan`
+--
+
+INSERT INTO `karyawan` (`ID`, `Nama`, `Jabatan`) VALUES
+(1, 'Monsieur Neuvillette', 'CEO'),
+(2, 'Il Capitano', 'Co - CEO');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `gaji`
+--
+ALTER TABLE `gaji`
+  ADD PRIMARY KEY (`ID_Gaji`),
+  ADD KEY `gaji_ibfk_1` (`ID_Karyawan`);
+
+--
+-- Indexes for table `karyawan`
+--
+ALTER TABLE `karyawan`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `gaji`
+--
+ALTER TABLE `gaji`
+  ADD CONSTRAINT `gaji_ibfk_1` FOREIGN KEY (`ID_Karyawan`) REFERENCES `karyawan` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
